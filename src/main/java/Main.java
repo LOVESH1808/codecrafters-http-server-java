1e93b881401aa44f795fe0dd0b48a79ac5eb19ac
LOVESH1808 <loveshdutta1808@gmail.com>
2024-03-28 16:45:42 +0530

    testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 public class Main {
   public static void main(String[] args) {
     ServerSocket serverSocket = null;
@@ -9,57 +8,13 @@ public class Main {
     try {
       serverSocket = new ServerSocket(4221);
       serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
-      InputStream inputStream = clientSocket.getInputStream();
-      BufferedReader bufferedReader =
-          new BufferedReader(new InputStreamReader(inputStream));
-      String startLine = bufferedReader.readLine();
-      String[] startLineArr = startLine.split(" ");
-      for(String s : startLineArr) {
-        System.out.println(s);
-      }
-      if (startLineArr[0].equals("GET")) {
-        if (startLineArr[1].equals("/")) {
-          clientSocket.getOutputStream().write(
-              ("HTTP/1.1 200 OK \r\n\r\n").getBytes(StandardCharsets.UTF_8));
-        } else if (startLineArr[1].startsWith("/echo")) {
-          String content = startLineArr[1].replace("/echo/", "");
-          StringBuilder stringBuilder = new StringBuilder();
-          stringBuilder.append("HTTP/1.1 200 OK \r\n");
-          stringBuilder.append("Content-Type: text/plain\r\n");
-          stringBuilder.append("Content-Length: ")
-              .append(content.length())
-              .append(" \r\n\r\n");
-          stringBuilder.append(content).append("\r\n\r\n");
-          clientSocket.getOutputStream().write(
-              (stringBuilder.toString()).getBytes(StandardCharsets.UTF_8));
-        } else if (startLineArr[1].equals("/user-agent")) {
-          String userAgentHeader = "";
-          while ((userAgentHeader = bufferedReader.readLine()) != null) {
-            System.out.println("while"+ "  "+ userAgentHeader);
-            if (userAgentHeader.startsWith("User-Agent")) {
-              break;
-            }
-          }
-          String[] userAgentHeaderArr = userAgentHeader.split(" ");
-          for(String s : userAgentHeaderArr) {
-            System.out.println(s);
-          }
-          StringBuilder stringBuilder = new StringBuilder();
-          stringBuilder.append("HTTP/1.1 200 OK \r\n");
-          stringBuilder.append("Content-Type: text/plain\r\n");
-          stringBuilder.append("Content-Length: ")
-              .append(userAgentHeaderArr[1].length())
-              .append(" \r\n");
-          stringBuilder.append(userAgentHeaderArr[1]).append("\r\n\r\n");
-          clientSocket.getOutputStream().write(
-              (stringBuilder.toString()).getBytes(StandardCharsets.UTF_8));
-        } else {
-          clientSocket.getOutputStream().write(
-              ("HTTP/1.1 404 Not Found \r\n\r\n")
-                  .getBytes(StandardCharsets.UTF_8));
-        }
+      while (true) {
+        clientSocket =
+            serverSocket.accept(); // Wait for connection from client.
+        System.out.println("accepted new connection");
+        ConnectionHandler connectionHandler =
+            new ConnectionHandler(clientSocket);
+        new Thread(connectionHandler).start();
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
