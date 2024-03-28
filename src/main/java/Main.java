0cbb59bf253214c156bfc73af937fbd074f0eb81
LOVESH1808 <loveshdutta1808@gmail.com>
2024-03-28 16:58:58 +0530

    testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,14 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 public class Main {
+  static String directoryPath = "";
   public static void main(String[] args) {
+    for (int i = 0; i < args.length; i++) {
+      if ("--directory".equals(args[i]) && i + 1 < args.length) {
+        directoryPath = args[i + 1];
+        break;
+      }
+    }
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
     try {
